b416ed7341dbdf1df26a117344eae127ca6c394f
gauthammk <gr8gautham@gmail.com>
2020-11-25 12:42:56 +0530

    fixed child_path error in 3 and 4 progressions

--- fetch_progressions.py
+++ fetch_progressions.py
@@ -1,6 +1,6 @@
 import requests
 import pandas as pd
-import json
+import time
 
 
 # read API key from file
@@ -28,8 +28,8 @@ def mineOneChordProgressions(baseUri, APIKey):
     # form the dataframe by reading the response as json
     oneChordProgressionsDf = pd.read_json(oneChordProgressionsJson)
 
-    # discard chord progressions with low probabilty(< 0.01) since very few songs have these progressions
-    lowProbabilityIndices = oneChordProgressionsDf[oneChordProgressionsDf['probability'] < 0.01].index
+    # discard chord progressions with low probabilty(< 0.05) since very few songs have these progressions
+    lowProbabilityIndices = oneChordProgressionsDf[oneChordProgressionsDf['probability'] < 0.05].index
     oneChordProgressionsDf.drop(lowProbabilityIndices, inplace=True)
     print(oneChordProgressionsDf)
 
@@ -51,7 +51,7 @@ def mineTwoChordProgressions(baseUri, APIKey):
 
     # get chord progression paths from file
     oneChordProgressionsDf = pd.read_csv('one_chord_progressions.csv')
-    oneChordProgressionPaths = oneChordProgressionsDf['chord_ID'].tolist()
+    oneChordProgressionPaths = oneChordProgressionsDf['child_path'].tolist()
 
     # loop through each of the chords in the one chord progression file
     for currentChordPath in oneChordProgressionPaths:
@@ -60,6 +60,7 @@ def mineTwoChordProgressions(baseUri, APIKey):
         twoChordProgressionsEndpoint = chordProgressionsEndpoint + \
             str(currentChordPath)
         print('Finding: ', twoChordProgressionsEndpoint)
+
         # get results
         response = requests.get(
             twoChordProgressionsEndpoint, headers=headers)
@@ -74,8 +75,8 @@ def mineTwoChordProgressions(baseUri, APIKey):
     # form the dataframe by reading the response as json
     twoChordProgressionsDf = pd.read_json(twoChordProgressionsJson)
 
-    # discard chord progressions with low probabilty(< 0.01) since very few songs have these progressions
-    lowProbabilityIndices = twoChordProgressionsDf[twoChordProgressionsDf['probability'] < 0.01].index
+    # discard chord progressions with low probabilty(< 0.05) since very few songs have these progressions
+    lowProbabilityIndices = twoChordProgressionsDf[twoChordProgressionsDf['probability'] < 0.05].index
     twoChordProgressionsDf.drop(lowProbabilityIndices, inplace=True)
     print(twoChordProgressionsDf)
 
@@ -83,8 +84,108 @@ def mineTwoChordProgressions(baseUri, APIKey):
     twoChordProgressionsDf.to_csv('two_chord_progressions.csv', index=False)
 
 
+def mineThreeChordProgressions(baseUri, APIKey):
+
+    # define endpoint
+    chordProgressionsEndpoint = baseUri + 'trends/nodes?cp='
+
+    # add API key as a Bearer token for authorization
+    headers = {"Authorization": "Bearer " + APIKey}
+
+    # define the list to store three chord progressions
+    threeChordProgressionsList = []
+
+    # get chord progression paths from file
+    twoChordProgressionsDf = pd.read_csv('two_chord_progressions.csv')
+    twoChordProgressionPaths = twoChordProgressionsDf['child_path'].tolist()
+
+    # loop through each of the chords in the two chord progression file
+    for currentChordPath in twoChordProgressionPaths:
+
+        # construct a request endpoint by appending to the chord in the file
+        threeChordProgressionsEndpoint = chordProgressionsEndpoint + \
+            str(currentChordPath)
+        print('Finding: ', threeChordProgressionsEndpoint)
+
+        # get results
+        response = requests.get(
+            threeChordProgressionsEndpoint, headers=headers)
+
+        # append each three chord progression to the string (after removing the '[]' brackets since we need to build the whole json object in one list)
+        threeChordProgressionsList.append(response.text[1:-1])
+
+    # manual transformation into json format
+    threeChordProgressionsJson = '[' + \
+        ','.join(threeChordProgressionsList) + ']'
+    print(threeChordProgressionsJson)
+
+    # form the dataframe by reading the response as json
+    threeChordProgressionsDf = pd.read_json(threeChordProgressionsJson)
+
+    # discard chord progressions with low probabilty(< 0.05) since very few songs have these progressions
+    lowProbabilityIndices = threeChordProgressionsDf[threeChordProgressionsDf['probability'] < 0.05].index
+    threeChordProgressionsDf.drop(lowProbabilityIndices, inplace=True)
+    print(threeChordProgressionsDf)
+
+    # write to file as csv
+    threeChordProgressionsDf.to_csv(
+        'three_chord_progressions.csv', index=False)
+
+
+def mineFourChordProgressions(baseUri, APIKey):
+
+    # define endpoint
+    chordProgressionsEndpoint = baseUri + 'trends/nodes?cp='
+
+    # add API key as a Bearer token for authorization
+    headers = {"Authorization": "Bearer " + APIKey}
+
+    # define the list to store four chord progressions
+    fourChordProgressionsList = []
+
+    # get chord progression paths from file
+    threeChordProgressionsDf = pd.read_csv('three_chord_progressions.csv')
+    threeChordProgressionPaths = threeChordProgressionsDf['child_path'].tolist(
+    )
+
+    # loop through each of the chords in the three chord progression file
+    for currentChordPath in threeChordProgressionPaths:
+
+        # construct a request endpoint by appending to the chord in the file
+        fourChordProgressionsEndpoint = chordProgressionsEndpoint + \
+            str(currentChordPath)
+        print('Finding: ', fourChordProgressionsEndpoint)
+
+        # get results
+        response = requests.get(
+            fourChordProgressionsEndpoint, headers=headers)
+
+        # append each four chord progression to the string (after removing the '[]' brackets since we need to build the whole json object in one list)
+        fourChordProgressionsList.append(response.text[1:-1])
+
+        # delay to bypass the rate limit
+        time.sleep(3)
+
+    # manual transformation into json format
+    fourChordProgressionsJson = '[' + ','.join(fourChordProgressionsList) + ']'
+    print(fourChordProgressionsJson)
+
+    # form the dataframe by reading the response as json
+    fourChordProgressionsDf = pd.read_json(fourChordProgressionsJson)
+
+    # discard chord progressions with low probabilty(< 0.05) since very few songs have these progressions
+    lowProbabilityIndices = fourChordProgressionsDf[fourChordProgressionsDf['probability'] < 0.05].index
+    fourChordProgressionsDf.drop(lowProbabilityIndices, inplace=True)
+    print(fourChordProgressionsDf)
+
+    # write to file as csv
+    fourChordProgressionsDf.to_csv('four_chord_progressions.csv', index=False)
+
+
 # main runner
 baseUri = 'https://api.hooktheory.com/v1/'
 APIKey = getAPIKey()
 # mineOneChordProgressions(baseUri, APIKey)
-mineTwoChordProgressions(baseUri, APIKey)
+# mineTwoChordProgressions(baseUri, APIKey)
+# mineThreeChordProgressions(baseUri, APIKey)
+# mineFourChordProgressions(baseUri, APIKey)
